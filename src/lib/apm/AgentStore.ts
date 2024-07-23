1d47e408807da5014e7a7bb9c47c3863fdbf5dc1
SunDawning <jobsimi@qq.com>
2024-07-23 13:12:07 +0800

    Add(apm/backend/AgentStore): Agent Store's agent CRUD API

--- src/lib/apm/AgentStore.ts
+++ src/lib/apm/AgentStore.ts
@@ -11,6 +11,66 @@ class AgentStore {
 		this.setBaseURL(baseURL);
 		this.setApiKey(apiKey);
 	}
+
+	// Agent Store's agent CRUD API:
+	// - login
+	// - create
+	// - edit
+	// - detail
+	// - search
+	// - delete
+	// - upload
+	async login(username, password) {
+		const response = await this.axios({
+			method: 'POST',
+			url: '/agentstore/agent/login',
+			data: { username, password },
+		});
+		this.setApiKey(response.data.sessionToken);
+	}
+	async create(payload) {
+		const response = await this.axios({
+			method: 'POST',
+			url: '/agentstore/agent/create',
+			data: {
+				name: payload.name,
+				version: payload.version,
+				label: payload.label,
+				description: payload.description,
+				icon: payload.icon,
+				doc: payload.doc,
+				config: payload.config,
+				executor: payload.executor,
+				md5: payload.md5,
+			},
+		});
+	}
+	async edit(payload) {
+		const response = await this.axios({
+			method: 'POST',
+			url: '/agentstore/agent/edit',
+			data: {
+				name: payload.name,
+				version: payload.version,
+
+				label: payload.label,
+				description: payload.description,
+				icon: payload.icon,
+				doc: payload.doc,
+				config: payload.config,
+				executor: payload.executor,
+				md5: payload.md5,
+			},
+		});
+	}
+	async upload(file) {
+		const response = await this.axios({
+			method: 'POST',
+			url: '/agentstore/agent/upload',
+			data: { file },
+		});
+	}
+
 	setApiKey(apiKey: string) {
 		if (!apiKey) {
 			return;
@@ -39,21 +99,6 @@ class AgentStore {
 		}
 
 		return false;
-	}
-	async login(username, password) {
-		const response = await this.axios({
-			method: 'POST',
-			url: '/agentstore/agent/login',
-			data: { username, password },
-		});
-		this.setApiKey(response.data.sessionToken);
-	}
-	async publish(file) {
-		const response = await this.axios({
-			method: 'POST',
-			url: '/agentstore/agent/publish',
-			data: { file },
-		});
 	}
 }
 

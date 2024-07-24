8a5e92dc06d6bbe797381e93200bb195a0a24e60
SunDawning <jobsimi@qq.com>
2024-07-24 17:22:18 +0800

    feat(APM/apm-python): 增加apm-python包, pip install apm-python

---
+++ apm-python/apm/APMAgent.py
@@ -0,0 +1,27 @@
+import requests
+
+
+class APMAgent:
+    def __init__(self):
+        pass
+
+    def save_output(self, saveconfig, status = {"done": True}, output = {}):
+        """
+        save output to apm server
+        """
+        url = saveconfig.get("url")
+        headers = saveconfig.get("headers")
+        
+        data = saveconfig.get("data")
+        data["output"] = output
+        data["status"] = status
+
+        # print("data", data)
+
+        try:
+            response = requests.post(url=url, headers=headers, json=data)
+            response_json = response.json()
+            print("response_json", response_json)
+            return response_json
+        except Exception as e:
+            print("Error while saving output to apm server: ", e)

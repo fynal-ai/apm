8c98f98d97652044dd627cbd69bf592d0f351374
SunDawning <jobsimi@qq.com>
2024-07-25 15:18:22 +0800

    feat(APM/apm-nodejs): 增加nodejs包

---
+++ apm-nodejs/src/APMAgent.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+class APMAgent {
+	constructor() {}
+	async saveOutput(saveconfig, status = { done: true }, output = {}) {
+		try {
+			const url = saveconfig['url'];
+			const headers = saveconfig['headers'];
+
+			const data = saveconfig['data'];
+			data['output'] = output;
+			data['status'] = status;
+
+			const response = await axios({
+				method: 'POST',
+				url,
+				headers,
+				data,
+			});
+			const responseJSON = response.data;
+			console.log('responseJSON', responseJSON);
+			return responseJSON;
+		} catch (error) {
+			console.log('Error while saving output to apm servier: ', error);
+		}
+	}
+}
+
+export { APMAgent };
---
+++ apm-nodejs/src/inde.js
@@ -0,0 +1,1 @@
+export { APMAgent } from './APMAgent.js';

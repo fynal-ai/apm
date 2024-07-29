8e17041a9dcb96db09aa82a89f03c9b483b31b49
SunDawning <jobsimi@qq.com>
2024-07-29 12:03:37 +0800

    feat(APM/apm-init): nodejs agent template

---
+++ apm-init/nodejs/src/Agent.js
@@ -0,0 +1,23 @@
+import { APMAgent } from '@jobsimi/apm';
+
+class Agent {
+	constructor() {
+		this.apmAgent = new APMAgent();
+	}
+	async run(params, saveconfig) {
+		console.log('Receive', params);
+
+		const text = { text: params.prompt };
+
+		// send to ChatGPT
+		const responseJSON = {};
+
+		const output = { text: '' };
+
+		this.apmAgent.saveOutput(saveconfig, undefined, output);
+
+		return output;
+	}
+}
+
+export { Agent };

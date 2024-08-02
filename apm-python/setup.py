d3157acc18d014acfb4ea6155f53a4c6e45c0d4f
SunDawning <jobsimi@qq.com>
2024-08-02 17:47:37 +0800

    perf(APM): jobsimi => fynal, fynal-ai

--- apm-python/setup.py
+++ apm-python/setup.py
@@ -5,14 +5,14 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     # 包的分发名称，使用字母、数字、_、-
-    name="jobsimi-apm-python",
+    name="fynal-apm-python",
     install_requires=["requests>=2.16.0"],
      # 版本号, 版本号规范：https://www.python.org/dev/peps/pep-0440/
-    version="0.0.4",
+    version="0.0.5",
     # 作者名
-    author="jobsimi",
+    author="fynal-ai",
      # 作者邮箱
-    author_email="jobsimi@qq.com",
+    author_email="fynalgpt@gmail.com",
     # 包的简介描述
     description="Agent Package Manager things",
     # 包的详细介绍(一般通过加载README.md)
@@ -20,7 +20,7 @@ setuptools.setup(
     # 和上条命令配合使用，声明加载的是markdown文件
     long_description_content_type="text/markdown",
     # 项目开源地址
-    url="https://github.com/jobsimi/apm",
+    url="https://github.com/fynal-ai/apm",
      # 如果项目由多个文件组成，我们可以使用find_packages()自动发现所有包和子包，而不是手动列出每个包，在这种情况下，包列表将是example_pkg
     packages=setuptools.find_packages(),
     # 关于包的其他元数据(metadata)

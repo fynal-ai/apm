31a0dfb9c39dc0734a7bbf529a58962bfc65545d
SunDawning <jobsimi@qq.com>
2024-08-09 11:54:46 +0800

    perf(APM/Backend/AgentResultConsumer): Try save output to callback server

--- src/lib/apm/AgentResultConsumer.ts
+++ src/lib/apm/AgentResultConsumer.ts
@@ -1,7 +1,7 @@
+import axios from 'axios';
 import { APMAgentServiceRun } from '../../database/models/APMAgentServiceRun.js';
 
 class AgentResultConsumer {
-	taskList: [] = [];
 	async IAmAlive(payload) {
 		const apmAgentServiceRuns = await APMAgentServiceRun.find({
 			'remoteRunSaveResultOption.url': payload.option.callback,
@@ -9,7 +9,42 @@ class AgentResultConsumer {
 			status: { $in: ['ST_DONE', 'ST_FAIL'] },
 		});
 
+		//
+		for (const apmAgentServiceRun of apmAgentServiceRuns) {
+			await this.saveOutputToCallbackServer(
+				apmAgentServiceRun.remoteRunSaveResultOption,
+				apmAgentServiceRun.output
+			);
+		}
+
 		return apmAgentServiceRuns;
+	}
+
+	async saveOutputToCallbackServer(config, output) {
+		// save output to run callback server
+		{
+			if (config?.url) {
+				console.log('Try save output to callback server');
+				try {
+					const url = config['url'];
+					const headers = config['headers'];
+
+					const data = config['data'];
+					data['output'] = output;
+
+					const response = await axios({
+						method: 'POST',
+						url,
+						headers,
+						data,
+					});
+					const responseJSON = response.data;
+					console.log('Callback server responseJSON', responseJSON);
+				} catch (error) {
+					console.log('Error while saving output to callback servier: ', error);
+				}
+			}
+		}
 	}
 }
 

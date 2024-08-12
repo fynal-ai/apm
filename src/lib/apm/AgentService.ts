a5db8b324aefbdcb66bb1fef54a636870abfd4bb
SunDawning <jobsimi@qq.com>
2024-08-12 09:23:27 +0800

    perf(APM/Backend/AgentService/Run): when run nodejs agent, use agentNameInPackageJSON

--- src/lib/apm/AgentService.ts
+++ src/lib/apm/AgentService.ts
@@ -265,10 +265,10 @@ class AgentService {
 		const packageJSON = await fs.readJson(
 			path.resolve(localRepositoryDir, 'agents', author, agentName, version, 'package.json')
 		);
-		// const agentNameInPackageJSON = packageJSON.name;
-		// console.log('agentNameInPackageJSON', agentNameInPackageJSON);
-		const mainScript = packageJSON.main;
-		console.log('mainScript', mainScript);
+		const agentNameInPackageJSON = packageJSON.name;
+		console.log('agentNameInPackageJSON', agentNameInPackageJSON);
+		// const mainScript = packageJSON.main;
+		// console.log('mainScript', mainScript);
 
 		const sh = `#!/bin/bash
 
@@ -289,11 +289,12 @@ if [ ! -f $PACKAGE_JSON_FILE ]; then
   "type": "module"
 }
 END
+  pnpm add ./${agentName};
 fi
 
 tee main.js <<END
 import fs from "fs";
-import { Agent } from "./${agentName}/${mainScript}";
+import { Agent } from "${agentNameInPackageJSON}";
 
 const input = JSON.parse(fs.readFileSync('input.json', 'utf8'));
 

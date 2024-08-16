75f6c7a63e67babf74dee8888650b8a6d46e4887
SunDawning <jobsimi@qq.com>
2024-08-16 14:42:50 +0800

    perf(APM/apm-nodejs/cli/inspect): /apm/agent/inspect

--- apm-nodejs/src/APMAgent.js
+++ apm-nodejs/src/APMAgent.js
@@ -126,6 +126,32 @@ class APMAgent {
 			console.log('Error while uninstalling apm agent: ', error.message);
 		}
 	}
+	async inspect(spec) {
+		// Try inspect agent
+		if (!spec) {
+			throw new Error('Agent spec is required. Try "apm --help"');
+		}
+
+		await this.loadConfig();
+
+		try {
+			const response = await axios({
+				method: 'POST',
+				url: '/apm/agent/inspect',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: this.apmAccessToken,
+				},
+				data: { spec },
+				baseURL: this.apmBaseURL,
+			});
+			const responseJSON = response.data;
+			console.log(responseJSON);
+			return responseJSON;
+		} catch (error) {
+			console.log('Error while inspect apm agent: ', error.message);
+		}
+	}
 	async list(payload) {
 		await this.loadConfig();
 
